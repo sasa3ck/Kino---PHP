e6fa01206c0a76bd50fe9a750728544b093c5825
Lex <sasa5ck@gmail.com>
2025-02-26 12:37:04 +0200

    fix

--- kernel/Upload/UploadedFile.php
+++ kernel/Upload/UploadedFile.php
@@ -16,7 +16,7 @@ class UploadedFile implements UploadedFileInterface
 
   public function move(string $path, string $fileName = null): string|false
   {
-    $storagePath = APP_PATH . "storage/$path";
+    $storagePath = APP_PATH . "/storage/$path";
 
     if (!is_dir($storagePath)) {
       mkdir($storagePath, 0777, true);
